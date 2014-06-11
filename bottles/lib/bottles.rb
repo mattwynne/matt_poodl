559a9e74bad54e48b30e48106d68ca3c0c780c06
Matt Wynne <matt@mattwynne.net>
2014-06-11 21:54:38 +0100

    Make the ugliness more obvious

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -4,16 +4,15 @@ puts instructions(__FILE__)
 class Bottles
 
   def verse(number)
-    if number > 2
+    case number
+    when 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" + "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" + "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" + "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
-    elsif number > 1
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" + "Take one down and pass it around, #{number-1} bottle of beer on the wall.\n"
-    elsif number == 1
-      "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" + "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    elsif number == 0
-      %{No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-}
     end
   end
 
